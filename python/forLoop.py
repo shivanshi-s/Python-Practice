339560083e81d4be204e1fb4aa27845e4ea3a444
shivanshi <shivanshi.saxena2@gmail.com>
2021-10-06 18:48:00 +0530

    Python practice

---
+++ python/forLoop.py
@@ -0,0 +1,16 @@
+# print('My name is')
+# for i in range(5):
+#       print('Jimmy 5 times ' + str(i))
+
+# total   = 0 
+# for num in range(101):
+#    total = num+total
+# print(total)
+
+# print('My name is')
+# for i in range(12,16):  #upto 16 but not including
+#       print('Jimmy 5 times ' + str(i))
+
+print('My name is')
+for i in range(5,1,-1):  #start stop step
+       print('Jimmy 5 times ' + str(i))
