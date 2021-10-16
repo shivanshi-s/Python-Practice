55d24a1e34cd243aebb372e8d4c157092b9f5900
shivanshi <shivanshi.saxena2@gmail.com>
2021-10-16 23:19:10 +0530

    python practice

---
+++ python/list_2.py
@@ -0,0 +1,18 @@
+for i in range(4):
+   print(i)
+
+list( range(4) )
+
+spam = list( range(0,100,2))
+print(spam)
+
+supplies = [ 'pens', 'pencils', 'erasers', 'scale' ]
+for i in range(len(supplies)):
+   print('Index ' + str(i)+ ' contains : ' + supplies[i] )
+
+
+size, color, disposition = 'big','black','skinny'
+# assigning
+
+#range function returns a list like value 
+#variables can be swapped using multiple assignment
