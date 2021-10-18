6c56e62a01aaf94310ff034f502dd85e08e4853f
shivanshi <shivanshi.saxena2@gmail.com>
2021-10-19 01:09:13 +0530

    more aout strings

---
+++ python/string_adv.py
@@ -0,0 +1,21 @@
+print('Say hi to Bob\'s cat!')
+print(r'This is carol\'s cat.') # raw string
+print("""Dear Alice,
+Eve's Cat has been arrested for catsortion, catnapping and cat burglary
+Sincerely, Bob. """)
+
+
+spam = 'Hello World'
+
+#indexes, slices, negative indexes, in and not in operators, case sensitive - kinda like lists.
+print('-------------------\n-------------------')
+
+# ------------------ STRING METHODS -------------------------------
+
+#string methods return new string values rather than modifying the string in place.
+# since strings are IMMUTABLE- cannot be modified anyway
+
+#upper, lower
+
+spam = "hello world"
+print(spam.upper())
