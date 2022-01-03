99940b155e57c6ba668a947be5d4abdb37a8660d
shivanshi <shivanshi.saxena2@gmail.com>
2022-01-03 13:30:37 +0530

    Completed the pong game

--- Mini-Projects/Pong/pong.py
+++ Mini-Projects/Pong/pong.py
@@ -1,4 +1,15 @@
 # Getting started
+# Classic Ping Pong Game using Python Turtle Library
+# Task Breakdown for the Game
+# Task 1- Build the game window and initialize the game's main loop
+# Task 2 : Create the two paddles and the ball
+# Task 3 : Move the paddles with the keyboard
+# Task 4 : Move the ball and constrain its movement
+# Task 5 : Bounce the ball back when it collides with the paddles
+# Task 6 : add a scoring system to the game
+
+# """************************************************************************"""
+
 import turtle
 
 wn = turtle.Screen()
@@ -7,11 +18,17 @@ wn.bgcolor("black")
 wn.setup(width = 800, height = 600)
 wn.tracer(0)
 
+
+# Score
+score_a = 0
+score_b = 0
+
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("white")
+paddle_a.color("green")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350,0)
@@ -21,7 +38,7 @@ paddle_a.goto(-350,0)
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
-paddle_b.color("white")
+paddle_b.color("green")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350,0)
@@ -30,11 +47,10 @@ paddle_b.goto(350,0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
-ball.color("white")
+ball.color("yellow")
 ball.penup()
-ball.goto(0,0)
-ball.dx  = 2
-ball.dy = -2  #everytime the ball moves, it moves 2 pixels
+ball.dx  = 0.15
+ball.dy = 0.15 #everytime the ball moves, it moves 2 pixels
 
 
 # Pen 
@@ -96,9 +112,16 @@ while True:
    if ball.xcor() > 390:
       ball.goto(0,0)
       ball.dx *= -1
+      score_a +=1
+      pen.clear()
+      pen.write("Player A: {} Player B: {}".format(score_a,score_b),align="center", font=("Courier",24,"normal"))
+
    if ball.xcor() < -390:
       ball.goto(0,0)
       ball.dx *= -1
+      score_b +=1
+      pen.clear()
+      pen.write("Player A: {} Player B: {}".format(score_a,score_b),align="center", font=("Courier",24,"normal"))
 
    #paddle and ball collisions
    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
